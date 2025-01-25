a67d17dc2a9cc011f5be4f89e6f7fc6eb6e4d43f
sixfortyfour <sixfortyfour@users.noreply.github.com>
2025-01-25 16:45:08 +0000

    Added logging

--- src/Sixfortyfour.DotNetEcho.HttpServer/Program.cs
+++ src/Sixfortyfour.DotNetEcho.HttpServer/Program.cs
@@ -1,16 +1,29 @@
+using Serilog;
 using Sixfortyfour.DotNetEcho.HttpServer;
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("/applogs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+Log.Information("Starting up");
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLogging();
 
 // Add services to the container.
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.Map("/echo", async (HttpRequest httpRequest) =>
 {
-    return await httpRequest.ToEchoResponseAsync();
+    var echoResponse = await httpRequest.ToEchoResponseAsync();
+
+    Log.Information<EchoResponse>($"{echoResponse.Method}", echoResponse);
+
+    return echoResponse;
 });
 
 app.Run();
